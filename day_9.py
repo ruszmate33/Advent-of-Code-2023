3d072ddd652af69920766c3a7f1f1be6f8824cd8
Mate Rusz <ruszmate33@gmail.com>
2024-01-17 07:16:18 +0100

    day 9 part 2 solved

--- day_9.py
+++ day_9.py
@@ -5,6 +5,8 @@ TEST_INPUT = """0 3 6 9 12 15
 # 114 for TEST_INPUT
 # 1702218515 part 1
 
+# 925 part 2
+
 
 def sequence_differences(sequence):
     return [j - i for i, j in zip(sequence, sequence[1:])]
@@ -25,14 +27,21 @@ def extrapolate_from_diff_sequences(seqs: list[list[int]]) -> int:
     return sum(seq[-1] for seq in seqs)
 
 
-def preceeding_value(seqs: list[list[int]]):
-    # for i in range(1, len(seqs) + 1):
-        # print(seqs[-1 + i][0], seqs[-1 + i])
-        # print(seqs[-i][0])
-    starting_values = [seqs[-i][0] for i in range(1, len(seqs) + 1)]
-    print(f'{starting_values=}')
-    back_extrapolated = sequence_differences(starting_values)
-    print(f'{back_extrapolated=}')
+def preceeding_value(seqs: list[list[int]]) -> int:
+    """
+    seq[4] - seq[3] + seq[2] - seq[1] + seq[0]
+    10 - 3 - 0 - 2 - 0 = 5
+    """
+    starting_values = [s[0] for s in seqs]
+    # print(f'{starting_values=}')
+    back_extrapolated = 0
+    for ind, value in enumerate(starting_values):
+        if ind % 2 == 0:
+            back_extrapolated += value
+        else:
+            back_extrapolated -= value
+    # print(f'{back_extrapolated=}')
+    return back_extrapolated
 
 
 def main():
@@ -40,20 +49,19 @@ def main():
         puzzle_input = f.read()
         # print(TEST_INPUT)
         sum_next_values = 0
-        # for history in puzzle_input.split('\n'):
-        for history in TEST_INPUT.split('\n'):
+        for history in puzzle_input.split('\n'):
+            # for history in TEST_INPUT.split('\n'):
             sequences_for_extrapolation = create_sequence_for_extrapolation(history)
-            print(f'{sequences_for_extrapolation=}')
-            
-            # part 1 
+            # print(f'{sequences_for_extrapolation=}')
+
+            # part 1
             # next_value = extrapolate_from_diff_sequences(sequences_for_extrapolation)
             # sum_next_values += next_value
 
             # part 2
-            preceeding_value(sequences_for_extrapolation)
+            sum_next_values += preceeding_value(sequences_for_extrapolation)
 
-
-        # print(sum_next_values)
+        print(sum_next_values)
 
 
 if __name__ == '__main__':
